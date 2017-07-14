6de41e583635b9a797c2d9e5bca56577baa54b9f
JaypeeD <akosijaypee29@gmail.com>
2017-07-14 16:07:25 +0800

    Update install_lamp.sh

--- install_lamp.sh
+++ install_lamp.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 #Install Apache2 Web Server, Lamp and MySQL Server
-echo 'Downloading package lists ...'
-sudo apt-get update;
 
 echo 'Installing Apache2 Web Server ...'
 sudo apt-get install apache2;
 
+sudo add-apt-repository ppa:ondrej/php;
+
+echo 'Downloading package lists ...'
+sudo apt-get update;
+
 echo 'Installing Php ...'
-sudo apt-get-repository ppa:ondrej/php;
 sudo apt-get install php7.0 libapache2-mod-php7.0 php7.0-mbstring php7.0-xml;
 
 echo 'Installing MySql Server ...'
